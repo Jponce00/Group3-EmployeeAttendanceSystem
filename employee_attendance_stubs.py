8ba7ef360b9597ef99b70f9541fe5953eeb0877d
lizie3 <lizandramedina15@gmail.com>
2026-07-01 11:47:56 -0400

    Added add_record() stub

--- employee_attendance_stubs.py
+++ employee_attendance_stubs.py
@@ -1,1 +1,29 @@
+"""
+Employee Attendance System - Function Stubs
 
+Team Members:
+- Johnny Ponce
+- Miluska Altamirano
+- Lizandra Medina
+- Star Valladares
+
+Course: COP 1047C - Introduction to Python Programming
+Date: July 1, 2026
+"""
+
+def add_record(records):
+    """
+    Add a new employee attendance record to the system.
+
+    Parameters:
+        records (list): The current list of employee attendance records.
+
+    Returns:
+        list: Updated list of employee attendance records with the new employee added.
+
+    Author: Lizandra Medina
+    Status: Stub - Implementation pending Phase 3
+    """
+
+    # TODO: Implementation in Phase 3
+    pass
